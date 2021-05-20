98dcde441fecc6f126afc91b63e7ecf48c9d4ef5
dpopche1 <dimitar.popchev1@globalfoundries.com>
2021-05-20 01:55:37 -0400

    update to modulino style

--- src/templates/skeleton.pl
+++ src/templates/skeleton.pl
@@ -6,3 +6,22 @@ use diagnostics;
 use feature qw/say/;
 
 use Data::Dumper;
+
+main() unless caller;
+
+sub main {
+    my $obj = MyClass->new;
+    $obj->hello;
+}
+
+package MyClass;
+
+use strict;
+use warnings;
+
+sub new { bless {} => shift };
+
+sub hello { print "Hello World\n" }
+
+no warnings 'void';
+"MyClass"
